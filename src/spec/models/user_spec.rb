7785026be33de5b0996e1df533f7af1d571394e4
Tomas Sedovic <tsedovic@redhat.com>
2010-03-29 11:27:55 +0200

    Add specs for user model.

--- src/spec/models/user_spec.rb
+++ src/spec/models/user_spec.rb
@@ -2,11 +2,45 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @valid_attributes = {
-    }
   end
 
   it "should create a new user 'tuser'" do
-    Factory.create(:tuser)
+    user = Factory.create(:tuser)
+    user.should be_valid
   end
+
+  it "should require password confirmation" do
+    user = User.new(Factory.attributes_for(:tuser))
+    user.should be_valid
+    user.password_confirmation = "different password"
+    user.should_not be_valid
+  end
+
+  it "should require unique login" do
+    user1 = Factory.create(:tuser)
+    user2 = Factory.create(:tuser)
+    user1.should be_valid
+    user2.should be_valid
+
+    user2.login = user1.login
+    user2.should_not be_valid
+  end
+
+  it "should require unique email" do
+    user1 = Factory.create(:tuser)
+    user2 = Factory.create(:tuser)
+    user1.should be_valid
+    user2.should be_valid
+
+    user2.email = user1.email
+    user2.should_not be_valid
+  end
+
+  it "should requive valid email" do
+    user = User.new(Factory.attributes_for(:tuser))
+
+    user.email = "invalid-email"
+    user.should_not be_valid
+  end
+
 end
