81a935a3404789ec778428916440f3a425a2834e
Tomas Sedovic <tsedovic@redhat.com>
2010-04-13 17:34:33 +0200

    Fix the failing Provider model tests.

--- src/spec/models/provider_spec.rb
+++ src/spec/models/provider_spec.rb
@@ -1,73 +1,77 @@
 require 'spec_helper'
 
 describe Provider do
-  before(:each) do
-    @provider = Factory.create(:mock_provider)
-    @client = mock('DeltaCloud', :null_object => true)
-    @provider.stub!(:connect).and_return(@client)
-  end
-
-  it "should return a client object" do
-    @provider.send(:valid_framework?).should be_true
-  end
-
-  it "should validate mock provider" do
-    @provider.should be_valid
-  end
-
-  it "should require a valid name" do
-    [nil, ""].each do |invalid_value|
-      @provider.name = invalid_value
-      @provider.should_not be_valid
+  context "(using stubbed out connect method)" do
+    before(:each) do
+      @client = mock('DeltaCloud', :null_object => true)
+      @provider = Factory.build(:mock_provider)
+      @provider.stub!(:connect).and_return(@client)
     end
-  end
 
-  it "should require a valid cloud_type" do
-    [nil, ""].each do |invalid_value|
-      @provider.cloud_type = invalid_value
-      @provider.should_not be_valid
+    it "should return a client object" do
+      @provider.send(:valid_framework?).should be_true
     end
-  end
 
-  it "should require a valid url" do
-    [nil, ""].each do |invalid_value|
-      @provider.url = invalid_value
-      @provider.should_not be_valid
+    it "should validate mock provider" do
+      @provider.should be_valid
     end
+
+    it "should require a valid name" do
+      [nil, ""].each do |invalid_value|
+        @provider.name = invalid_value
+        @provider.should_not be_valid
+      end
+    end
+
+    it "should require a valid cloud_type" do
+      [nil, ""].each do |invalid_value|
+        @provider.cloud_type = invalid_value
+        @provider.should_not be_valid
+      end
+    end
+
+    it "should require a valid url" do
+      [nil, ""].each do |invalid_value|
+        @provider.url = invalid_value
+        @provider.should_not be_valid
+      end
+    end
+
+    it "should be able to connect to the specified framework" do
+      @provider.should be_valid
+      @provider.connect.should_not be_nil
+
+      @provider.url = "http://invalid.provider/url"
+      @provider.stub(:connect).and_return(nil)
+      deltacloud = @provider.connect
+      @provider.should have(1).error_on(:url)
+      @provider.errors.on(:url).should eql("must be a valid provider url")
+      @provider.should_not be_valid
+      deltacloud.should be_nil
+    end
+
+    it "should require unique name" do
+      @provider.save!
+      provider2 = Factory.build :mock_provider
+      provider2.stub!(:connect).and_return(@client)
+      provider2.should be_valid
+
+      provider2.name = @provider.name
+      provider2.should_not be_valid
+    end
+
   end
 
-  it "should require unique name" do
-    provider1 = Factory.create :mock_provider
-    provider2 = Factory.create :mock_provider
-    provider1.should be_valid
-    provider2.should be_valid
+  context "(using original connect method)" do
+    it "should log errors when connecting to invalid url" do
+      @logger = mock('Logger', :null_object => true)
+      @provider = Factory.build(:mock_provider)
+      @provider.stub!(:logger).and_return(@logger)
 
-    provider2.name = provider1.name
-    provider2.should_not be_valid
-  end
-
-  it "should be able to connect to the specified framework" do
-    @provider.should be_valid
-    @provider.connect.should_not be_nil
-
-    @provider.url = "http://invalid.provider/url"
-    @provider.stub(:connect).and_return(nil)
-    deltacloud = @provider.connect
-    @provider.should have(1).error_on(:url)
-    @provider.errors.on(:url).should eql("must be a valid provider url")
-    @provider.should_not be_valid
-    deltacloud.should be_nil
-  end
-
-  it "should log errors when connecting to invalid url" do
-    @logger = mock('Logger', :null_object => true)
-    @provider = Factory.create(:mock_provider)
-    @provider.stub!(:logger).and_return(@logger)
-
-    @provider.should be_valid
-    @provider.logger.should_receive(:error).twice
-    @provider.url = "http://invalid.provider/url"
-    @provider.connect.should be_nil
+      @provider.logger.should_receive(:error).twice
+      @provider.url = "http://invalid.provider/url"
+      @provider.connect.should be_nil
+    end
   end
 
 end
