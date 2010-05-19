f7fd446b2895f4d5820f66aacbe3e31d41240d5e
Tomas Sedovic <tsedovic@redhat.com>
2010-05-19 16:08:49 +0200

    Stub the pages linked from the navigation.
    Added Dashboard and the subpages (Summary, Alerts, Service Quality, Quota
    Usage, Billing and Help Tickets), Instances, Templates and Settings.

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,4 +1,4 @@
-# 
+#
 # Copyright (C) 2009 Red Hat, Inc.
 # Written by Scott Seago <sseago@redhat.com>
 #
@@ -20,7 +20,7 @@
 ActionController::Routing::Routes.draw do |map|
 
   # The priority is based upon order of creation: first created -> highest priority.
-  
+
   # Sample of regular route:
   # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
@@ -29,7 +29,7 @@ ActionController::Routing::Routes.draw do |map|
   # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
   # This route can be invoked with purchase_url(:id => product.id)
 
-  # You can have the root of your site routed by hooking up '' 
+  # You can have the root of your site routed by hooking up ''
   # -- just remember to delete public/index.html.
 
 
@@ -44,6 +44,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :account, :controller => "users"
   map.resources :users
   map.root  :login
+
+  map.dashboard '/dashboard', :controller => 'dashboard'
+  map.instance '/instance', :controller => 'instance'
+  map.templates '/templates', :controller => 'templates'
+  map.settings '/settings', :controller => 'settings'
 
   # Temporarily disable this route, provider stuff is not restful yet.
   # Will be re-enabled in upcoming patch
