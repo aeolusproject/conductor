eacb17722e8d6dc91700489d1caca1b6a7604c35
Jan Provaznik <jprovazn@redhat.com>
2010-06-04 09:30:48 +0200

    dependency on nokogiri and will_paginate
    - nokogiri and will_paginate add to rpm spec
    - nokogori gem dep added to environment.rb

--- src/config/environment.rb
+++ src/config/environment.rb
@@ -46,6 +46,7 @@ Rails::Initializer.run do |config|
   config.gem "deltacloud-client", :lib => "deltacloud"
   config.gem "haml"
   config.gem "will_paginate"
+  config.gem "nokogiri", :version => ">= 1.4.0"
 
   config.active_record.observers = :instance_observer, :task_observer
   # Only load the plugins named here, in the order given. By default, all plugins
