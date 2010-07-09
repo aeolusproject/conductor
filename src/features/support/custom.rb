f61d3ed56796098806179e5648494cda9df6063b
Jason Guiditta <jason.guiditta@gmail.com>
2010-07-09 17:40:24 -0400

    Unhook the cucumber tests from a running core server.
    We may want (and probably do) a truly full stack test
    that has all external components running, but that is not
    the goal with the cucumber tests.  External dependencies
    should be tested in the 3rd-party library, so for our
    acceptance testing, just assume those kinds of things
    work as advertised.
    
    Again, a full 'integrated' test suite should probably be
    incorporated at some point as well.

--- src/features/support/custom.rb
+++ src/features/support/custom.rb
@@ -3,3 +3,18 @@ Fixtures.reset_cache
 fixtures_folder = File.join(Rails.root, 'spec', 'fixtures')
 fixtures = Dir[File.join(fixtures_folder, '*.yml')].map {|f| File.basename(f, '.yml') }
 Fixtures.create_fixtures(fixtures_folder, fixtures)
+
+# We can't stub out these methods properly in cucumber, and we don't want to
+# couple these tests to require the core server be running (connections should be tested
+# in the client code), so override the methods for tests here.
+Provider.class_eval do
+  def valid_framework?
+    true
+  end
+end
+
+CloudAccount.class_eval do
+  def valid_credentials?
+    true
+  end
+end
