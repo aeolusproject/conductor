f1c55e78a4610c1bf795ec33338df2e3f665ebb0
Jason Guiditta <jguiditt@redhat.com>
2010-09-13 17:40:44 -0400

    Zone Factory was not loading correctly, this should fix it.

--- src/spec/factories/metadata_object.rb
+++ src/spec/factories/metadata_object.rb
@@ -1,5 +1,5 @@
 Factory.define :default_zone_metadata, :class => MetadataObject  do |o|
   o.key 'default_zone'
-  o.value Factory.create(:zone).id
+  o.value {Factory.create(:zone).id}
   o.object_type 'Zone'
 end
