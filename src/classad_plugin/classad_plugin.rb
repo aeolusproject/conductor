f3fbd46ddcecc08dff1bc964c8d6b7f32b0ff02e
Chris Lalancette <clalance@redhat.com>
2010-09-27 15:00:23 -0400

    Make the classad plugin parse the /etc/sysconfig/deltacloud-rails
    This is so that it can properly fetch the value of RAILS_ENV
    and access the correct database.
    
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- src/classad_plugin/classad_plugin.rb
+++ src/classad_plugin/classad_plugin.rb
@@ -8,13 +8,24 @@ require 'instance'
 require 'cloud_account'
 require 'pool'
 require 'quota'
+require 'parseconfig'
 
 def classad_plugin(logf, conf_path, instance_key, account_id)
-  ENV['RAILS_ENV'] = 'development' unless ENV['RAILS_ENV']
+  rails_env = "development"
+  begin
+    config = ParseConfig.new('/etc/sysconfig/deltacloud-rails')
+    env = config.get_value("RAILS_ENV")
+    if not env.nil?
+      rails_env = env
+    end
+  rescue
+    # if any of this failed, then just assume the development database and
+    # hope for the best
+  end
 
   logf.puts "loading db config from #{conf_path}"
   conf = YAML::load(File.open(conf_path))
-  ActiveRecord::Base.establish_connection(conf[ENV['RAILS_ENV']])
+  ActiveRecord::Base.establish_connection(conf[rails_env])
 
   # I left the puts in here because you can run condor_negotiator -f from the
   # command line and it will print this stuff out.  Very nice for debugging.
