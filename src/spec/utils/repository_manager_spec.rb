37f4e3e1da641de5a87260cfa8557babbeab88d6
Jan Provaznik <jprovazn@redhat.com>
2010-10-14 16:30:51 +0200

    Removed another obsolete rspec test
    Is obsolete because of xmls will be downloaded with rake task.

--- src/spec/utils/repository_manager_spec.rb
+++ src/spec/utils/repository_manager_spec.rb
@@ -18,8 +18,6 @@ describe RepositoryManager do
       Typhoeus::Response.new(:code => 200, :body => @packagegroups_json))
     hydra.stub(:get, "http://pulptest/repositories/jboss/packages/").and_return(
       Typhoeus::Response.new(:code => 200, :body => @packages_json))
-    hydra.stub(:get, "http://nonexisting.repo/").and_return(
-      Typhoeus::Response.new(:code => 404))
 
     @rmanager = RepositoryManager.new(:config => [{
       'baseurl' => 'http://pulptest',
@@ -37,13 +35,5 @@ describe RepositoryManager do
     rep = @rmanager.repositories.first
     rep.groups.keys.sort.should == ["JBoss Core Packages", "JBoss Drools",
       "JBoss Social Networking Web Application"]
-  end
-
-  it "should raise exception when downloading of repomd fails" do
-    @rmanager = RepositoryManager.new(:config => [{
-      'baseurl' => 'http://nonexisting.repo/',
-      'type'    => 'xml',
-    }])
-    lambda { @rmanager.all_groups}.should raise_error
   end
 end
