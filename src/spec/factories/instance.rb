3f0c9a24a3f0e358cffac44cc9ba708b1ba4a057
Jason Guiditta <jguiditt@redhat.com>
2010-10-18 15:00:35 -0400

    BZ #642432 - test 'determine the key in use for an instance'.
    https://bugzilla.redhat.com/show_bug.cgi?id=642432
    
    If the instance has a key you should see a download link,
    if not, there should be no link.

--- src/spec/factories/instance.rb
+++ src/spec/factories/instance.rb
@@ -9,10 +9,20 @@ Factory.define :instance do |i|
   i.state "running"
 end
 
-Factory.define :pending_instance, :parent => :instance do |i|
+Factory.define :mock_running_instance, :parent => :instance do |i|
+  i.instance_key { |k| k.association(:mock_instance_key)}
+end
+
+Factory.define :mock_pending_instance, :parent => :instance do |i|
   i.state Instance::STATE_PENDING
 end
 
 Factory.define :new_instance, :parent => :instance do |i|
   i.state Instance::STATE_NEW
+end
+
+Factory.define :ec2_instance, :parent => :instance do |i|
+  i.association :hardware_profile, :factory => :ec2_hwp1
+  i.association :cloud_account, :factory => :ec2_cloud_account
+  i.association :instance_key, :factory => :ec2_instance_key1
 end
