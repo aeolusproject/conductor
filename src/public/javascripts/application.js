6436712449d01a39bb78af58f2400f62dcdf1bb9
Jakub Steiner <jsteiner@redhat.com>
2010-10-22 18:34:32 +0200

    Initial styling of selected packages as an inline list, inspired by suse studio.
    - removed all the line noise. Using whitespace as a separator.
    - bug: removing a packae seems to remove all
    - restructured the styling to be in the #build context
    - use links for package names and include a hidden field so the package selection is actually stored.
    - Use .live() for package removal and dropdowns
    - readd functionality - Add all packages if the collection checkbox is checked.
    - show package details when clicking on a name. Always shown when nojavascript
    
    applies on next: 1e487e84813b219464b85765d06696177641f1fa

--- src/public/javascripts/application.js
+++ src/public/javascripts/application.js
@@ -112,6 +112,31 @@ var Aggregator = {
       }
     });
   };
+  $.fn.positionAncestor = function(selector) {
+    var left = 0;
+    var top = 0;
+    this.each(function(index, element) {
+        // check if current element has an ancestor matching a selector
+        // and that ancestor is positioned
+        var $ancestor = $(this).closest(selector);
+        if ($ancestor.length && $ancestor.css("position") !== "static") {
+            var $child = $(this);
+            var childMarginEdgeLeft = $child.offset().left - parseInt($child.css("marginLeft"), 10);
+            var childMarginEdgeTop = $child.offset().top - parseInt($child.css("marginTop"), 10);
+            var ancestorPaddingEdgeLeft = $ancestor.offset().left + parseInt($ancestor.css("borderLeftWidth"), 10);
+            var ancestorPaddingEdgeTop = $ancestor.offset().top + parseInt($ancestor.css("borderTopWidth"), 10);
+            left = childMarginEdgeLeft - ancestorPaddingEdgeLeft;
+            top = childMarginEdgeTop - ancestorPaddingEdgeTop;
+            // we have found the ancestor and computed the position
+            // stop iterating
+            return false;
+        }
+    });
+    return {
+        left:    left,
+        top:    top
+    }
+};
 })(jQuery);
 
 /* Aggregator JS */
