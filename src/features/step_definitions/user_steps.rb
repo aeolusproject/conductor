4ef71d81421c8f596d284e21b5ad9757fff0c054
Martyn Taylor <mtaylor@redhat.com>
2010-11-16 14:47:55 +0000

    BZ#643096 Added test for cancel button on new user page

--- src/features/step_definitions/user_steps.rb
+++ src/features/step_definitions/user_steps.rb
@@ -1,3 +1,11 @@
 Given /^there is a user "([^"]*)"$/ do |name|
-  @user = User.find_by_login(name)
+ User.find_by_login(name).should_not == nil
+end
+
+Given /^there are (\d+) users$/ do |number|
+  User.all.size.should == number.to_i
+end
+
+Then /^there should only be (\d+) users$/ do |number|
+  User.all.size.should == number.to_i
 end
