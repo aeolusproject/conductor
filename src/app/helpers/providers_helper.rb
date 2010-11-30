79c23b2fe32488e1d77c40c0384c7dfd822a5404
Martyn Taylor <mtaylor@redhat.com>
2010-11-30 12:22:36 +0000

    Redmine#8 Update Provider Links on Accounts Page

--- src/app/helpers/providers_helper.rb
+++ src/app/helpers/providers_helper.rb
@@ -1,7 +1,7 @@
 module ProvidersHelper
 
   def edit_button(provider, action)
-    if provider and ['show', 'accounts'].include? action
+    if provider and action == 'show'
       link_to 'Edit', edit_provider_path(provider), :class => 'button'
     else
       content_tag('a', 'Edit', :href => '#', :class => 'button disabled')
