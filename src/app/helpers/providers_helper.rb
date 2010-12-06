d8ac804def47f732b2880593feaaf9e9caaab6d6
Jozef Zigmund <jzigmund@redhat.com>
2010-12-06 16:48:09 +0100

    Providers-add AJAX to edit-FIXED BUG
    This patch fixed bug in page for Adding Provider accounts.
    It was caused by using link_to with the same class of a href tag, so i've added
    id='edit_button' to editing provider's link.

--- src/app/helpers/providers_helper.rb
+++ src/app/helpers/providers_helper.rb
@@ -2,7 +2,7 @@ module ProvidersHelper
 
   def edit_button(provider, action)
     if provider and action == 'show'
-      link_to 'Edit', edit_provider_path(provider), :class => 'button'
+      link_to 'Edit', edit_provider_path(provider), :class => 'button', :id => 'edit_button'
     else
       content_tag('a', 'Edit', :href => '#', :class => 'button disabled')
     end
