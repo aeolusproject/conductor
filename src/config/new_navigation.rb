aac36d69ab51e49222bc3265eed3bb329a45eb78
Tomas Sedovic <tsedovic@redhat.com>
2010-12-10 13:06:02 +0100

    Add a functional layout for the new UI
    This hooks the navigation to the placeholder controllers, making it work.
    Next, it provides a basic layout with header, footer and the three pivotal
    panes for the content to come.
    
    Plus some stylesheet love to set the proper placement.

--- src/config/new_navigation.rb
+++ src/config/new_navigation.rb
@@ -1,27 +1,25 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.autogenerate_item_ids = false
   navigation.items do |first_level|
-    first_level.item :resource_management_view, t(:resource_management_view), '#' do |second_level|
-      second_level.item :pools, t(:pools), '#'
-      second_level.item :deployments, t(:deployments),'#'
-      second_level.item :instances, t(:instances), '#'
-      second_level.item :searches, t(:searches), '#'
+    first_level.item :resource_management, t(:resource_management), resources_pools_path, :highlights_on => /\/resources/ do |second_level|
+      second_level.item :pools, t(:pools), resources_pools_path
+      second_level.item :deployments, t(:deployments),resources_deployments_path
+      second_level.item :instances, t(:instances), resources_instances_path
     end
-    first_level.item :image_factory_view, t(:image_factory_view), '#' do |second_level|
-      second_level.item :templates, t(:templates), '#'
-      second_level.item :assemblies, t(:assemblies),'#'
-      second_level.item :deployables, t(:deployables), '#'
-      second_level.item :template_collections, t(:template_collections), '#'
+    first_level.item :image_factory, t(:image_factory), image_factory_templates_path, :highlights_on => /\/image_factory/ do |second_level|
+      second_level.item :templates, t(:templates), image_factory_templates_path
+      second_level.item :assemblies, t(:assemblies), image_factory_assemblies_path
+      second_level.item :deployables, t(:deployables), image_factory_deployables_path
     end
-    first_level.item :administration, t(:admin), '#' do |second_level|
-      second_level.item :users, t(:users), '#'
-      second_level.item :roles, t(:roles), '#'
-      second_level.item :cloud_providers, t(:cloud_providers), '#'
-      second_level.item :provider_account, t(:provider_account), '#'
-      second_level.item :cloud_engine_hardware_profiles, t(:cloud_engine_hardware_profiles), '#'
-      second_level.item :cloud_engine_realms, t(:cloud_engine_realms), '#'
-      second_level.item :pool_families, t(:pool_families), '#'
-      second_level.item :settings, t(:setting), '#'
+    first_level.item :administration, t(:admin), admin_users_path, :highlights_on => /\/admin/ do |second_level|
+      second_level.item :users, t(:users), admin_users_path
+      second_level.item :roles, t(:roles), admin_roles_path
+      second_level.item :providers, t('providers.providers'), admin_providers_path
+      second_level.item :provider_accounts, t(:provider_accounts), admin_provider_accounts_path
+      second_level.item :hardware_profiles, t(:cloud_engine_hardware_profiles), admin_hardware_profiles_path
+      second_level.item :realms, t(:cloud_engine_realms), admin_realms_path
+      second_level.item :pool_families, t(:pool_families), admin_pool_families_path
+      second_level.item :settings, t(:setting), admin_settings_path
     end
     first_level.item :dashboard, t(:dashboard), '#'
   end
