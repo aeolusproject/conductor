e6ec7ab190d7ef0426dedfa667145c2478e88d12
Jan Provaznik <jprovazn@redhat.com>
2010-12-15 17:14:06 +0100

    Instance launching in new UI (views + controller action)

--- src/app/controllers/resources/instances_controller.rb
+++ src/app/controllers/resources/instances_controller.rb
@@ -2,6 +2,76 @@ class Resources::InstancesController < ApplicationController
   before_filter :require_user
 
   def new
+    @instance = Instance.new(params[:instance])
+    #require_privilege(Privilege::INSTANCE_MODIFY, @instance.pool) if @instance.pool
+
+    unless @instance.template
+      redirect_to select_template_resources_instances_path
+      return
+    end
+
+    @pools = Pool.list_for_user(@current_user, Privilege::INSTANCE_MODIFY)
+    @realms = Realm.find(:all, :conditions => { :provider_id => nil })
+    @hardware_profiles = HardwareProfile.all(
+      :include => :architecture,
+      :conditions => {
+        :provider_id => nil,
+        'hardware_profile_properties.value' => @instance.template.architecture
+      }
+    )
+  end
+
+  def select_template
+    # FIXME: we need to list only templates for particular user,
+    # => TODO: add TEMPLATE_* permissions
+    @templates = Template.paginate(
+      :page => params[:page] || 1,
+      :include => {:images => :replicated_images},
+      :conditions => "replicated_images.uploaded = 't'"
+    )
+  end
+
+  def create
+    if params[:cancel]
+      redirect_to select_template_resources_instances_path
+      return
+    end
+
+    @instance = Instance.new(params[:instance])
+    @instance.state = Instance::STATE_NEW
+    @instance.owner = current_user
+
+    require_privilege(Privilege::INSTANCE_MODIFY,
+                      Pool.find(@instance.pool_id))
+    #FIXME: This should probably be in a transaction
+    if @instance.save
+        @task = InstanceTask.new({:user        => current_user,
+                                  :task_target => @instance,
+                                  :action      => InstanceTask::ACTION_CREATE})
+        if @task.save
+          condormatic_instance_create(@task)
+          if Quota.can_start_instance?(@instance, nil)
+            flash[:notice] = "Instance added."
+          else
+            flash[:warning] = "Quota Exceeded: Instance will not start until you have free quota"
+          end
+        else
+          @pool = @instance.pool
+          render :new
+        end
+      redirect_to resources_instances_path
+    else
+      @pools = Pool.list_for_user(@current_user, Privilege::INSTANCE_MODIFY)
+      @realms = Realm.find(:all, :conditions => { :provider_id => nil })
+      @hardware_profiles = HardwareProfile.all(
+        :include => :architecture,
+        :conditions => {
+          :provider_id => nil,
+          'hardware_profile_properties.value' => @instance.template.architecture
+        }
+      )
+      render :new
+    end
   end
 
   def edit
@@ -21,7 +91,6 @@ class Resources::InstancesController < ApplicationController
       {:name => 'PUBLIC ADDRESS', :sort_attr => 'public_addresses'},
       {:name => 'PROVIDER', :sortable => false},
       {:name => 'CREATED BY', :sort_attr => 'users.last_name'},
-      {:name => '', :sortable => false},
     ]
 
     pools = Pool.list_for_user(@current_user, Privilege::INSTANCE_MODIFY)
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -34,7 +34,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.namespace 'resources' do |r|
     r.resources :pools, :deployments
-    r.resources :instances, :collection => {:start => :get, :stop => :get}
+    r.resources :instances, :collection => {:start => :get, :stop => :get, :select_template => :get}
   end
 
   map.namespace 'image_factory' do |r|
