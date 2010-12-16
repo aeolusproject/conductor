16a534b1ac670ea2cfa8c9f547ccee40c9fd90ce
Ladislav Martincik <lmartinc@redhat.com>
2010-12-16 16:07:10 +0100

    Support new UI in Cucumber.

--- src/features/step_definitions/web_steps.rb
+++ src/features/step_definitions/web_steps.rb
@@ -12,6 +12,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "pat
 # Commonly used webrat steps
 # http://github.com/brynary/webrat
 
+Given /^I am using new UI$/ do
+  cookies['layout'] = 'newui'
+end
+
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
 end
