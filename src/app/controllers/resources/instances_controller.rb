cb532cff5a5c1f0db33993e7ddea16cddf999050
Jan Provaznik <jprovazn@redhat.com>
2010-12-22 10:22:20 +0100

    New UI - show instance in the pane

--- src/app/controllers/resources/instances_controller.rb
+++ src/app/controllers/resources/instances_controller.rb
@@ -79,21 +79,6 @@ class Resources::InstancesController < ApplicationController
   end
 
   def index
-    @header = [
-      {:name => 'VM NAME', :sort_attr => 'name'},
-      {:name => 'STATUS', :sortable => false},
-      {:name => 'TEMPLATE', :sort_attr => 'templates.name'},
-      {:name => 'PUBLIC ADDRESS', :sort_attr => 'public_addresses'},
-      {:name => 'PROVIDER', :sortable => false},
-      {:name => 'CREATED BY', :sort_attr => 'users.last_name'},
-    ]
-
-    pools = Pool.list_for_user(@current_user, Privilege::INSTANCE_MODIFY)
-    @instances = Instance.all(
-      :include => [:template, :owner],
-      :conditions => {:pool_id => pools},
-      :order => (params[:order_field] || 'name') +' '+ (params[:order_dir] || 'asc')
-    )
   end
 
   def key
@@ -147,6 +132,24 @@ class Resources::InstancesController < ApplicationController
         :provider_id => nil,
         'hardware_profile_properties.value' => @instance.template.architecture
       }
+    )
+  end
+
+  def load_instances
+    @header = [
+      {:name => 'VM NAME', :sort_attr => 'name'},
+      {:name => 'STATUS', :sortable => false},
+      {:name => 'TEMPLATE', :sort_attr => 'templates.name'},
+      {:name => 'PUBLIC ADDRESS', :sort_attr => 'public_addresses'},
+      {:name => 'PROVIDER', :sortable => false},
+      {:name => 'CREATED BY', :sort_attr => 'users.last_name'},
+    ]
+
+    pools = Pool.list_for_user(@current_user, Privilege::INSTANCE_MODIFY)
+    @instances = Instance.all(
+      :include => [:template, :owner],
+      :conditions => {:pool_id => pools},
+      :order => (params[:order_field] || 'name') +' '+ (params[:order_dir] || 'asc')
     )
   end
 end
