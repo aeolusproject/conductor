d37b06e17e29317b1366f4dcbde8b7846ccdaded
Chris Lalancette <clalance@redhat.com>
2011-04-05 11:12:59 -0400

    Remove the unused tests.
    According to Jay Guiditta, the tests in src/test are no longer
    used.  In the interests of keeping a tidy codebase, remove them.
    
    Signed-off-by: Chris Lalancette <clalance@redhat.com>

--- src/test/functional/instance_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class InstanceControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
--- src/test/unit/quota_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class QuotaTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
