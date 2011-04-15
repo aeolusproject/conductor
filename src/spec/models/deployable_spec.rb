ce0c966f6d094412086cc91cdbb392f98507daee
Tomas Sedovic <tsedovic@redhat.com>
2011-04-15 16:00:31 +0200

    Add tests for deployable validation

--- src/spec/models/deployable_spec.rb
+++ src/spec/models/deployable_spec.rb
@@ -22,4 +22,35 @@ describe Deployable do
     d.assemblies.size.should eql(1)
   end
 
+  it "should not be destroyable when it has running instances" do
+    deployable = Factory.create(:deployable)
+    deployment = Factory.create(:deployment, :deployable_id => deployable.id)
+    assembly = Factory.create(:assembly)
+
+    instance = Factory.create(:instance, :deployment_id => deployment.id, :assembly_id => assembly.id, :template_id => nil)
+    Deployable.find(deployable).should_not be_destroyable
+
+    instance.state = Instance::STATE_STOPPED
+    instance.save!
+    Deployable.find(deployable).should be_destroyable
+  end
+
+  it "should not be destroyable when it has stopped stateful instances" do
+    deployable = Factory.build(:deployable)
+    deployment = Factory.build(:deployment, :deployable_id => deployable.id)
+    deployable.deployments << deployment
+    assembly = Factory.build(:assembly)
+
+    instance = Factory.build(:instance, :deployment_id => deployment.id, :assembly_id => assembly.id, :template_id => nil)
+    instance.stub!(:restartable?).and_return(true)
+    deployment.instances << instance
+    deployable.should_not be_destroyable
+
+    instance.state = Instance::STATE_STOPPED
+    deployable.should_not be_destroyable
+
+    instance.stub!(:restartable?).and_return(false)
+    deployable.should be_destroyable
+  end
+
 end
