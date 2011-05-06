a8d0f73693859b63d9831da607d9bdc6093b9afb
Mark McLoughlin <markmc@redhat.com>
2011-05-06 16:02:01 +0100

    Avoid adding a non-null column with a null default value
    I was getting this error when running 'rake db:migrate' with sqlite:
    
      SQLite3::SQLException: Cannot add a NOT NULL column with default value NULL: ALTER TABLE "deployables" ADD "uuid" varchar(255) NOT NULL
    
    To avoid the warning, we can add the column as nullable and then change
    it to non-null later.
    
    Presumably this would fail if we ran the migration on a DB containing
    any data in the deployables table, but this is unlikely. To avoid it,
    we could do e.g.
    
        Deployable.reset_column_information
        Deployable.all.each do |deployable|
          deployable.uuid = "" if deployable.uuid.nil?
          deployable.xml = "" if deployable.xml.nil?
        end
    
    but that seems like overkill given the situation, especially since the
    default values don't make any sense.

--- src/db/migrate/20110207100800_update_deployables.rb
+++ src/db/migrate/20110207100800_update_deployables.rb
@@ -2,12 +2,15 @@ class UpdateDeployables < ActiveRecord::Migration
   def self.up
     change_table :legacy_deployables do |t|
       t.integer :lock_version, :default => 0
-      t.string  :uuid, :null => false
-      t.binary  :xml, :null => false
+      t.string  :uuid
+      t.binary  :xml
       t.string  :uri
       t.text    :summary
       t.boolean :uploaded, :default => false
     end
+
+    change_column :legacy_deployables, :uuid, :string, :null => false
+    change_column :legacy_deployables, :xml, :string, :null => false
 
     create_table :legacy_assemblies_legacy_deployables, :id => false do |t|
       t.integer :legacy_assembly_id,  :null => false
