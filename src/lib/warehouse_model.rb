0b8e452ccf75d645aad0576649616bbf16f03fa6
Matt Wagner <matt.wagner@redhat.com>
2011-06-10 16:55:44 -0400

    Implements ==, first, and last operators on WarehouseClient.

--- src/lib/warehouse_model.rb
+++ src/lib/warehouse_model.rb
@@ -5,6 +5,17 @@ class BucketNotFound < Exception;end
 class WarehouseModel
   WAREHOUSE_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/image_warehouse.yml")
 
+  def ==(other_obj)
+    # If the objects have different instance variables defined, they're definitely not ==
+    return false unless instance_variables.sort == other_obj.instance_variables.sort
+    # Otherwise, ensure that they're all the same
+    instance_variables.each do |iv|
+      return false unless other_obj.instance_variable_get(iv) == instance_variable_get(iv)
+    end
+    # They have the same instance variables and values, so they're equal
+    true
+  end
+
   class << self
     attr_accessor :warehouse, :bucket, :bucket_name
 
@@ -17,15 +28,29 @@ class WarehouseModel
       end
     end
 
-    def all
+    def bucket_objects
       self.set_warehouse_and_bucket if self.bucket.nil?
 
       begin
-        self.bucket.objects.map do |wh_object|
-          self.new(wh_object.attrs(wh_object.attr_list))
-        end
+        self.bucket.objects
       rescue RestClient::ResourceNotFound
         []
+      end
+    end
+
+    def first
+    obj = bucket_objects.first
+      obj ? self.new(obj.attrs(obj.attr_list)) : nil
+    end
+
+    def last
+      obj = bucket_objects.last
+      obj ? self.new(obj.attrs(obj.attr_list)) : nil
+    end
+
+    def all
+      bucket_objects.map do |wh_object|
+          self.new(wh_object.attrs(wh_object.attr_list))
       end
     end
 
---
+++ src/spec/models/warehouse_model_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe WarehouseModel do
+
+  before do
+    create_test_iwhd_data_for("builds")
+  end
+
+  describe "==" do
+    it "should be true for cloned object" do
+      image1 = Image.new({:uuid => '123', :foo => 'bar'})
+      image2 = image1.dup
+      image1.should == image2
+    end
+
+    it "should be true for two identical lookups" do
+      image1 = Image.first
+      image2 = Image.first
+      image1.should == image2
+    end
+
+    it "should be false for a changed object" do
+      image1 = Image.new({:uuid => '123', :foo => 'bar'})
+      image2 = image1.dup
+      image2.foo = 'baz'
+      image1.should_not == image2
+    end
+
+    it "should be false for objects with a different number of attributes" do
+      image1 = Image.new({:uuid => '123', :foo => 'bar'})
+      image2 = Image.new({:uuid => '123'})
+      image1.should_not == image2
+      image2.should_not == image1
+    end
+
+  end
+
+end
