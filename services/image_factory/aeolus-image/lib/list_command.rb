e8b85299cc09a880558260741565cd1855ad0d4d
Martyn Taylor <mtaylor@redhat.com>
2011-06-30 13:04:42 +0100

    Implemented aoelus-image list providers

--- services/image_factory/aeolus-image/lib/list_command.rb
+++ services/image_factory/aeolus-image/lib/list_command.rb
@@ -23,11 +23,35 @@ module Aeolus
       end
 
       def providers
-        not_implemented
+        print_values = [["NAME", "TYPE", "URL"]]
+
+        doc = Nokogiri::XML conductor['/providers'].get
+        doc.xpath("/providers/provider").each do |provider|
+          print_values << [provider.xpath("name").text, provider.xpath("provider_type").text, provider.xpath("url").text]
+        end
+
+        format_print(print_values)
+        quit(0)
       end
 
       def accounts
         not_implemented
+      end
+
+      private
+      # Takes a 2D array of strings and neatly prints them to STDOUT
+      def format_print(print_values)
+        widths =  Array.new(print_values[0].size, 0)
+        print_values.each do |print_value|
+          widths = widths.zip(print_value).map! {|width, value| value.length > width ? value.length : width }
+        end
+
+        print_values.each do |print_value|
+          widths.zip(print_value) do |width, value|
+            printf("%-#{width + 5}s", value)
+          end
+          puts ""
+        end
       end
     end
   end
