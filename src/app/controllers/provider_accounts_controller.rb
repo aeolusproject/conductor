c177349b0acdd04d1a4aace7bec4085d9cf353d5
Jiri Tomasek <jtomasek@redhat.com>
2011-07-11 13:46:14 +0200

    Provider Name column added to Provider Accounts table

--- src/app/controllers/provider_accounts_controller.rb
+++ src/app/controllers/provider_accounts_controller.rb
@@ -190,6 +190,7 @@ class ProviderAccountsController < ApplicationController
       { :name => '', :sortable => false },
       { :name => t("provider_accounts.index.provider_account_name"), :sortable => false },
       { :name => t("provider_accounts.index.username"), :sortable => false},
+      { :name => t("provider_accounts.index.provider_name"), :sortable => false },
       { :name => t("provider_accounts.index.provider_type"), :sortable => false },
       { :name => t("provider_accounts.index.quota_used"), :sortable => false },
       { :name => t("provider_accounts.index.quota_limit"), :sortable => false },
