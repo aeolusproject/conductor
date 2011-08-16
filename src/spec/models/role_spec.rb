7987c556385f1a1c086767aba9f920c415f72c6f
Tomas Hrcka <thrcka@redhat.com>
2011-08-16 14:00:31 +0200

    role spec fix

--- src/spec/models/role_spec.rb
+++ src/spec/models/role_spec.rb
@@ -7,6 +7,6 @@ describe Role do
     u.name = ('a' * 256)
     u.valid?.should be_false
     u.errors[:name].should_not be_nil
-    u.errors[:name].should =~ /^is too long.*/
+    u.errors[:name][0].should =~ /^is too long.*/
   end
 end
