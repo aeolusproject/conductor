30b0d90930c792d1255191d3c16d4cdaef1ae05d
Jiri Tomasek <jtomasek@redhat.com>
2011-09-02 15:21:36 +0200

    primary tab navigation uses simple_navigation gem

--- src/config/navigation.rb
+++ src/config/navigation.rb
@@ -19,12 +19,9 @@
 SimpleNavigation::Configuration.run do |navigation|
   navigation.autogenerate_item_ids = false
   navigation.items do |first_level|
-    first_level.item :resource_management, t(:resource_management), pools_path, :highlights_on => /^\/$/ do |second_level|
-      second_level.item :pools, t('pools.index.pools'), pools_path
-      second_level.item :deployments, t('deployments.deployments'),deployments_path, :highlights_on => /^\/$|\/deployments/
-      second_level.item :instances, t("instances.instances"), instances_path
-    end
-    first_level.item :administration, t(:administration), users_path, :highlights_on => /\/admin/ do |second_level|
+    first_level.dom_class = 'container'
+    first_level.item :monitor, t(:monitor), pools_path, :class => 'monitor', :highlights_on => lambda { ["pools" ,"deployments", "instances"].include? controller_name }
+    first_level.item :administer, t(:administer), users_path, :class => 'administer', :highlights_on => /\/admin/ do |second_level|
       second_level.item :users, t('users.users'), users_path, :highlights_on => /\/users/
       second_level.item :roles, t('roles.roles'), roles_path, :highlights_on => /\/roles/
       second_level.item :providers, t('providers.providers'), providers_path, :highlights_on => /\/providers/
@@ -36,6 +33,5 @@ SimpleNavigation::Configuration.run do |navigation|
       second_level.item :catalog_entries, t('catalog_entries.index.catalog_entries'), catalog_entries_path, :highlights_on => /\/catalog_entries/
       second_level.item :settings, t('settings.settings'), settings_path, :highlights_on => /\/settings/
     end
-    first_level.item :dashboard, t(:dashboard), '#'
   end
 end
