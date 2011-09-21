253a21108ab8645874e89759016106ab0ac41065
Jiri Tomasek <jtomasek@redhat.com>
2011-09-21 10:38:10 +0200

    Catalogs and catalog entries adopt new admin_ui

--- src/config/navigation.rb
+++ src/config/navigation.rb
@@ -24,7 +24,7 @@ SimpleNavigation::Configuration.run do |navigation|
     first_level.item :administer, t(:administer), users_path, :class => 'administer' do |second_level|
       second_level.item :users_and_groups, "Users & Groups", users_path, :link => { :class => 'users' }, :highlights_on => /\/users/
       second_level.item :environments, "Environments", settings_path, :link => { :class => 'environments' }, :highlights_on => /\/settings/
-      second_level.item :content, "Content", settings_path, :link => { :class => 'content' }, :highlights_on => /\/settings/
+      second_level.item :content, "Content", catalogs_path, :link => { :class => 'content' }, :highlights_on => /\/catalogs|\/catalog_entries/
       second_level.item :cloud_providers, "Cloud Providers", providers_path, :link => { :class => 'providers' }, :highlights_on => /\/providers/
     end
   end
