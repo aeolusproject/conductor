a3f6c4b9cff46b76cd661ae7023b29a4a3586897
Martyn Taylor <mtaylor@redhat.com>
2011-10-01 11:01:35 +0100

    Added nokogiri require

--- src/config/application.rb
+++ src/config/application.rb
@@ -42,7 +42,7 @@ else
   require 'thin'
   require 'json'
   require 'fastercsv'
-  require 'active_support/core_ext'
+  require 'nokogiri'
   #require 'railties'
 
   unless (ENV["RAILS_ENV"] == "production")
