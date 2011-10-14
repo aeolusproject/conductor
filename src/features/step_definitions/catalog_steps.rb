de9b1891c5b235696c627e45335e909bae5b0516
Scott Seago <sseago@redhat.com>
2011-10-14 11:44:51 -0400

    Fixed failing catalog entries cucumber test.

--- src/features/step_definitions/catalog_steps.rb
+++ src/features/step_definitions/catalog_steps.rb
@@ -2,7 +2,7 @@ Given /^there is a "([^"]*)" catalog$/ do |name|
   FactoryGirl.create :catalog, :name => name
 end
 
-When /^I check "([^"]*)" catalog/ do |arg1|
+When /^I check "([^"]*)" catalog$/ do |arg1|
   catalog = Catalog.find_by_name(arg1)
   check("catalog_checkbox_#{catalog.id}")
 end
