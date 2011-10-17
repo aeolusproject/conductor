3520bb581dff1e870d659981c30f36771d47a785
Jozef Zigmund <jzigmund@redhat.com>
2011-10-17 18:23:50 +0200

    Revised navigation strings for i18n

--- src/config/navigation.rb
+++ src/config/navigation.rb
@@ -20,13 +20,13 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.autogenerate_item_ids = false
   navigation.items do |first_level|
     first_level.dom_class = 'container'
-    first_level.item :monitor, t(:monitor), pools_path, :class => 'monitor', :link => { :id => 'monitor' }, :highlights_on => /\/deployments|\/pools|\/instances|\/\z/
-    first_level.item :administer, t(:administer), users_path, :class => 'administer' do |second_level|
-      second_level.item :users_and_groups, "Users & Groups", users_path, :link => { :class => 'users' }, :highlights_on => /\/users|\/roles|\/account/
-      second_level.item :environments, "Environments", pool_families_path, :link => { :class => 'environments' }, :highlights_on => /\/pool_families/
-      second_level.item :content, "Content", catalogs_path, :link => { :class => 'content' }, :highlights_on => /\/catalogs|\/catalog_entries|\/realms|\/hardware_profiles|\/realm_mappings/
-      second_level.item :cloud_providers, "Cloud Providers", providers_path, :link => { :class => 'providers' }, :highlights_on => /\/providers/
-      second_level.item :settings, "Settings", settings_path, :link => { :class => 'settings'}, :highlights_on => /\/settings/
+    first_level.item :monitor, t('navigation.first_level.monitor'), pools_path, :class => 'monitor', :link => { :id => 'monitor' }, :highlights_on => /\/deployments|\/pools|\/instances|\/\z/
+    first_level.item :administer, t('navigation.first_level.administer'), users_path, :class => 'administer' do |second_level|
+      second_level.item :users_and_groups, t('navigation.second_level.users_groups'), users_path, :link => { :class => 'users' }, :highlights_on => /\/users|\/roles|\/account/
+      second_level.item :environments, t('navigation.second_level.environments'), pool_families_path, :link => { :class => 'environments' }, :highlights_on => /\/pool_families/
+      second_level.item :content, t('navigation.second_level.content'), catalogs_path, :link => { :class => 'content' }, :highlights_on => /\/catalogs|\/catalog_entries|\/realms|\/hardware_profiles|\/realm_mappings/
+      second_level.item :cloud_providers, t('navigation.second_level.cloud_providers'), providers_path, :link => { :class => 'providers' }, :highlights_on => /\/providers/
+      second_level.item :settings, t('navigation.second_level.settings'), settings_path, :link => { :class => 'settings'}, :highlights_on => /\/settings/
     end
   end
 end
