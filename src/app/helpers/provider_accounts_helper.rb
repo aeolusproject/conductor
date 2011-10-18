10fdd79f854eb506f3816a416e26c9f84ae34478
Jirka Tomasek <jtomasek@redhat.com>
2011-10-18 15:18:32 +0200

    Flat table styling according to new design

--- src/app/helpers/provider_accounts_helper.rb
+++ src/app/helpers/provider_accounts_helper.rb
@@ -1,13 +1,14 @@
 module ProviderAccountsHelper
   def provider_accounts_header
     [
-      { :name => '', :sortable => false },
+      { :name => '', :sortable => false, :class => 'checkbox' },
+      { :name => '', :sortable => false, :class => 'alert' },
       { :name => t("provider_accounts.index.provider_account_name"), :sortable => false },
       { :name => t("provider_accounts.index.username"), :sortable => false},
       { :name => t("provider_accounts.index.provider_name"), :sortable => false },
       { :name => t("provider_accounts.index.provider_type"), :sortable => false },
-      { :name => t("provider_accounts.index.quota_used"), :sortable => false },
-      { :name => t("provider_accounts.index.quota_limit"), :sortable => false }
+      { :name => t("provider_accounts.index.quota_used"), :sortable => false, :class => 'center' },
+      { :name => t("provider_accounts.index.quota_limit"), :sortable => false, :class => 'center' }
     ]
   end
 end
