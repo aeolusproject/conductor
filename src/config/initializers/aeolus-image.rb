5ff5abd528b396ae6cdcfeff9316b8bd23bfeb38
Matt Wagner <matt.wagner@redhat.com>
2011-10-20 14:48:45 -0400

    aeolus-image initializer includes OAuth setup.
    In addition to adding support for Image Factory OAuth configuration,
    this uses a less-hackish syntax for our Warehouse configuration, and
    tries to be smart about warning about errors or raising on serious
    configuration errors.

--- src/config/initializers/aeolus-image.rb
+++ src/config/initializers/aeolus-image.rb
@@ -16,7 +16,42 @@
 # MA  02110-1301, USA.  A copy of the GNU General Public License is
 # also available at http://www.gnu.org/copyleft/gpl.html.
 
-config = YAML.load_file("#{::Rails.root.to_s}/config/settings.yml")
-Aeolus::Image::Warehouse::WarehouseModel.send(:class_variable_set, '@@config', config)
+begin
+  config = YAML.load_file("#{::Rails.root.to_s}/config/settings.yml")
+rescue Errno::ENOENT
+  raise "Failed to load #{::Rails.root.to_s}/config/settings.yml for aeolus-image-rubygem"
+end
 
-Aeolus::Image::Factory::Base.site = config[:imagefactory][:url]
+# Aeolus::Image::Warehouse takes our whole YAML config file:
+begin
+  Aeolus::Image::Warehouse::WarehouseModel.config = config
+rescue NoMethodError
+  raise "aeolus-image-rubygem is too old to support Warehouse configuration"
+end
+
+# Aeolus::Image::Factory has a more dignified configuration setting:
+begin
+  # Image Factory URL is required:
+  factory_site = config[:imagefactory][:url] or
+    raise "#{::Rails.root.to_s}/config/settings.yml failed to define Image Factory URL"
+
+  # Consumer key and secret are not required; we fall back into OAuth-less mode:
+  consumer_key    = config[:imagefactory][:oauth][:consumer_key] rescue nil
+  consumer_secret = config[:imagefactory][:oauth][:consumer_secret] rescue nil
+  factory_config = {}
+  if consumer_key && consumer_secret
+    factory_config = {
+      :site => factory_site,
+      :consumer_key => consumer_key,
+      :consumer_secret => consumer_secret
+    }
+  else
+    Rails.logger.warn "#{::Rails.root.to_s}/config/settings.yml did not define OAuth configuration for Image Factory; continuing without OAuth support"
+    factory_config = {
+      :site => factory_site
+    }
+  end
+  Aeolus::Image::Factory::Base.config = factory_config
+rescue NoMethodError
+  raise "aeolus-image-rubygem is too old to support Image Factory configuration"
+end
