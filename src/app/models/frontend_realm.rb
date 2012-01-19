a74a5f869af81318876f48b84190f69df7054948
Tzu-Mainn Chen <tzumainn@redhat.com>
2012-01-19 14:50:20 -0500

    BZ 783050 added description length validation

--- src/app/models/frontend_realm.rb
+++ src/app/models/frontend_realm.rb
@@ -45,6 +45,8 @@ class FrontendRealm < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
 
+  validates_length_of :description, :maximum => 255, :allow_blank => true
+
   PRESET_FILTERS_OPTIONS = []
 
   private
