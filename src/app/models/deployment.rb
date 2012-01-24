d4ad58b487165254539072347862908fcad976f6
Imre Farkas <ifarkas@redhat.com>
2012-01-24 13:19:03 +0100

    BZ770622: Filter for provider_type name on the list of deployments
    https://bugzilla.redhat.com/show_bug.cgi?id=770622

--- src/app/models/deployment.rb
+++ src/app/models/deployment.rb
@@ -464,9 +464,12 @@ class Deployment < ActiveRecord::Base
   private
 
   def self.apply_search_filter(search)
+    # TODO: after upgrading to 3.1 the SQL join statement can be done in Rails way by adding a has_many association to providers through provider_accounts
+    #       (Rails before version 3.1 does not support nested associations with through param)
     if search
       includes(:pool, :provider_accounts => :provider).
-          where("pools.name ILIKE :search OR deployments.name ILIKE :search OR providers.name ILIKE :search",
+          joins("LEFT OUTER JOIN provider_types ON provider_types.id = providers.provider_type_id").
+          where("pools.name ILIKE :search OR deployments.name ILIKE :search OR provider_types.name ILIKE :search",
                 :search => "%#{search}%")
     else
       scoped
