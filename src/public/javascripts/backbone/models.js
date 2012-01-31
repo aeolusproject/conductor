80bdb30af355c5563dae518b47a5505eb8c2e154
Imre Farkas <ifarkas@redhat.com>
2012-01-31 10:54:50 +0100

    Fixing syntax error in model.js

--- src/public/javascripts/backbone/models.js
+++ src/public/javascripts/backbone/models.js
@@ -2,12 +2,12 @@ Conductor.Models = Conductor.Models || {}
 
 
 Conductor.Models.Pool = Backbone.Model.extend({
-  urlRoot: Conductor.prefixedPath('/pools')
+  urlRoot: Conductor.prefixedPath('/pools'),
 
   initialize: function() {
     this.deployments = new Conductor.Models.Deployments().filter(function(attributes) {
-        return attributes.pool.id == this.id;
-        });
+      return attributes.pool.id == this.id;
+    });
   }
 });
 
