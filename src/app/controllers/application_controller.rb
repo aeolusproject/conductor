4420870b009cdfda039d8e2a8a604df25294ad72
Jozef Zigmund <jzigmund@redhat.com>
2012-02-13 17:03:40 +0100

    Internationalized breadcrumb controllers' names.

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -239,7 +239,8 @@ class ApplicationController < ActionController::Base
       breadcrumbs.slice!(index, breadcrumbs.length)
     end
     read_breadcrumbs
-    breadcrumbs.push({:name => name, :path => path, :viewstate => viewstate, :class => controller_name})
+    name = t("breadcrumbs.#{name}") if self.controller_name.eql?(name)
+    breadcrumbs.push({:name => name, :path => path, :viewstate => viewstate, :class => self.controller_name})
 
     session[:breadcrumbs] = breadcrumbs
   end
