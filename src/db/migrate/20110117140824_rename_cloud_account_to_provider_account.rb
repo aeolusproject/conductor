3611ad40976616b076250a691886bb661d6d0662
Steve Linabery <slinaber@redhat.com>
2012-02-20 11:28:25 -0600

    Revert "remove space before parenthesis in methods calls, breaks ruby 1.9 parser"
    This reverts commit 8b9ae8d98143dbd74511827eb7f89ab732946058.

--- src/db/migrate/20110117140824_rename_cloud_account_to_provider_account.rb
+++ src/db/migrate/20110117140824_rename_cloud_account_to_provider_account.rb
@@ -16,14 +16,14 @@
 
 class RenameCloudAccountToProviderAccount < ActiveRecord::Migration
   def self.up
-    rename_table(:cloud_accounts,:provider_accounts)
-    remove_column(:providers, :cloud_type)
-    add_column(:providers, :provider_type, :integer)
+    rename_table (:cloud_accounts,:provider_accounts)
+    remove_column (:providers, :cloud_type)
+    add_column (:providers, :provider_type, :integer)
   end
 
   def self.down
-    remove_column(:providers, :provider_type)
-    add_column(:providers, :cloud_type, :string, :null => false)
-    rename_table(:provider_accounts, :cloud_accounts)
+    remove_column (:providers, :provider_type)
+    add_column (:providers, :cloud_type, :string, :null => false)
+    rename_table (:provider_accounts, :cloud_accounts)
   end
 end
