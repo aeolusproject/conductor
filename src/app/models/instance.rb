23b0f5b057ba4dded17847a81df90fca2e3f29fa
Matt Wagner <matt.wagner@redhat.com>
2012-02-28 10:57:48 -0500

    Architecture is displayed for imported images.
    Part of https://bugzilla.redhat.com/show_bug.cgi?id=795743

--- src/app/models/instance.rb
+++ src/app/models/instance.rb
@@ -369,7 +369,7 @@ class Instance < ActiveRecord::Base
     # try to get architecture of the image associated with this instance
     # for imported images template is empty -> architecture is not set,
     # in this case we omit this check
-    return image.os.arch
+    return image.architecture
   rescue
     logger.warn "failed to get image architecture for instance '#{name}', skipping architecture check: #{$!}"
     logger.warn $!.backtrace.join("\n  ")
