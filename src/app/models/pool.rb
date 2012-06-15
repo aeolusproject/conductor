22ffc7b96288a21b09f558c852309488025df0a3
Scott Seago <sseago@redhat.com>
2012-06-15 11:29:01 -0400

    bug 831057: fix ambiguous order by name
    https://bugzilla.redhat.com/show_bug.cgi?id=831057
    
    There was a deployments 'order by' clause that just listed 'name'
    which was ambiguous (since the joined pool and pool_families models
    also have a 'name' column)

--- src/app/models/pool.rb
+++ src/app/models/pool.rb
@@ -124,7 +124,7 @@ class Pool < ActiveRecord::Base
 
     if options[:with_deployments]
       result[:deployments] = deployments.list_for_user(options[:current_user], Privilege::VIEW).
-                                         order('name asc').
+                                         order('deployments.name asc').
                                          paginate(:page => options[:page], :per_page => options[:per_page]).
                                          map {|d| d.as_json}
     end
