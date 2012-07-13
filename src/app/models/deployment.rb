2ea37f1dbe5dee5877bc39dee73f39950518d61f
Jan Provaznik <jprovazn@redhat.com>
2012-07-13 14:01:48 +0200

    Revert "838726 (1) - mark 'new' instances as create_failed if deployment launch fails"
    This reverts commit 2b76d1dc92f20771565197014fab830609b7eebd.
    
    I've accidentaly pushed this patch, reverting it for now.

--- src/app/models/deployment.rb
+++ src/app/models/deployment.rb
@@ -654,10 +654,9 @@ class Deployment < ActiveRecord::Base
     if Instance::ACTIVE_FAILED_STATES.include?(instance.state)
       # if this instance stop failed, whole deployment rollback failed
       self.state = STATE_ROLLBACK_FAILED
-      cleanup_failed_launch
     elsif instance.state == Instance::STATE_RUNNING
       deployment_rollback
-    elsif instances.all? {|i| i.inactive? or i.state == Instance::STATE_NEW}
+    elsif instances.all? {|i| i.inactive?}
       # some other instances might be failed (because their
       # launch failed), but it shouldn't be a problem if all
       # running instances stopped correctly
@@ -695,7 +694,6 @@ class Deployment < ActiveRecord::Base
       end
     end
     if error_occured
-      cleanup_failed_launch
       self.state = STATE_ROLLBACK_FAILED
       save!
     end
@@ -709,12 +707,6 @@ class Deployment < ActiveRecord::Base
         :status_code => self.state,
         :summary => "State changed to #{self.state}"
       )
-    end
-  end
-
-  def cleanup_failed_launch
-    instances.in_new_state.each do |instance|
-      instance.update_attribute(:state, Instance::STATE_CREATE_FAILED)
     end
   end
 end
