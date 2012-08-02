af4ee7737333992c045b59070149d83d24d5698f
Imre Farkas <ifarkas@redhat.com>
2012-08-02 14:24:57 +0200

    ProviderPriorityGroup related migrations

---
+++ src/db/migrate/20120710134428_create_provider_priority_group_elements.rb
@@ -0,0 +1,30 @@
+#
+#   Copyright 2012 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+class CreateProviderPriorityGroupElements < ActiveRecord::Migration
+  def self.up
+    create_table :provider_priority_group_elements do |t|
+      t.references :provider_priority_group, :null => false
+      t.references :value, :null => false, :polymorphic => true
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :provider_priority_group_elements
+  end
+end
