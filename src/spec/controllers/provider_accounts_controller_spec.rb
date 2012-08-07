56c29e736d23767451a4b9fd39db59eebcc8a1c2
Petr Blaho <pblaho@redhat.com>
2012-08-07 15:39:44 +0200

    Moves common shared examples for provider accounts API to their own file (cherry picked from commit 36d02fb4026db7e92f6241bf665ce6db9fb745a9)

--- src/spec/controllers/provider_accounts_controller_spec.rb
+++ src/spec/controllers/provider_accounts_controller_spec.rb
@@ -20,40 +20,6 @@ describe ProviderAccountsController do
 
   render_views
 
-  shared_examples_for "having XML with provider accounts" do
-    # TODO: implement more attributes checks
-    subject { Nokogiri::XML(response.body) }
-    context "list of provider accounts" do
-      let(:xml_provider_accounts) { subject.xpath('//provider_accounts/provider_account') }
-      context "number of provider accounts" do
-        it { xml_provider_accounts.size.should be_eql(number_of_provider_accounts) }
-      end
-      it "should have correct provider accounts" do
-        provider_accounts.each do |provider_account|
-          xml_provider_account = xml_provider_accounts.xpath("//provider_account[@id=\"#{provider_account.id}\"]")
-          xml_provider_account.xpath('name').text.should be_eql(provider_account.name.to_s)
-          xml_provider_account.xpath('@href').text.should be_eql(api_provider_account_url(provider_account))
-        end
-      end
-      it "should have not incorrect provider accounts" do
-        other_provider_accounts.each do |provider_account|
-          xml_provider_account = xml_provider_accounts.xpath("//provider_account[@id=\"#{provider_account.id}\"]")
-          xml_provider_account.should be_empty
-        end
-      end
-    end
-  end
-
-  shared_examples_for "having correct set of credentials" do
-    it "should be correct" do
-      provider_account.credentials.each do |credential|
-        label = credential.credential_definition.label
-        value = credential.value
-        xml_provider_account.xpath('//' + label).text.should be_eql(value)
-      end
-    end
-  end
-
   context "UI" do
 
     fixtures :all
