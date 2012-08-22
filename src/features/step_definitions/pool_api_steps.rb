ab2a01d9504a8460303c149b8f61ea6a52025c15
Crag Wolfe <cwolfe@redhat.com>
2012-08-22 12:36:25 -0400

    Remove name attribute from the pools index api endpoint
    As stated by Martin, all of our collection endpoints should include id
    and href attributes only, any other information should be found in a
    detail endpoint (in this case, the name is available in the detail for a
    given pool).
    
    Signed-off-by: Richard Su <rwsu@redhat.com>

--- src/features/step_definitions/pool_api_steps.rb
+++ src/features/step_definitions/pool_api_steps.rb
@@ -11,17 +11,10 @@ Then /^I should receive list of (\d+) pools as XML$/ do |number|
   pool_node_set = xml_body.xpath('//pools/pool')
   pool_node_set.size.should be_eql(number.to_i)
   ids = []
-  names = []
   pool_node_set.each() do |ns|
     ids << ns.get_attribute('id')
-    names << ns.get_attribute('name')
   end
-  ids.uniq!
-  names.sort!
   # verify unique id's
+  ids.uniq!
   ids.size.should be_eql(number.to_i)
-  if number == 3
-    # verify pool names match those given in the scenario
-    names.should be_eql(["Default", "bar", "foo"])
-  end
 end
