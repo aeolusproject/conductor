fface3c9733901d4cf1329c7c2c5539b8e1590fb
Scott Seago <sseago@redhat.com>
2012-09-05 12:04:33 -0400

    bug 784108: Hide additional links for non-privileged users.
    https://bugzilla.redhat.com/show_bug.cgi?id=784108
    
    "Images Valid"/"Repair Images" links are disabled if the user doesn't have edit permission on the deployable
    On the deployables list, the "add catalog" form filters the dropdown by catalogs that the user has permission to add to, and the form is hidden entirely if the user doesn't have edit rights on the deployable
    
    On the Environments index page, the add image links and edit pool links are hidden for users without permission to perform those actions
    
    Signed-off-by: Matt Wagner <matt.wagner@redhat.com>
    
    Conflicts:
    	src/app/views/pool_families/_list.html.haml

--- src/app/helpers/deployables_helper.rb
+++ src/app/helpers/deployables_helper.rb
@@ -1,15 +1,22 @@
 module DeployablesHelper
-  def image_valid?(assembly)
+  def image_valid?(assembly, modify_perm)
     edit_xml_path = edit_polymorphic_path([@catalog, @deployable], :edit_xml => true)
 
     if @missing_images.empty? && assembly[:hwp_name].present?
-      link_to t('.images_valid'), edit_xml_path, :class => 'images_valid', :id => 'edit_xml_button'
+      link_hash = {:label => '.images_valid', :class => 'images_valid'}
     elsif assembly[:hwp_name].nil?
-      link_to t('.repair_images'), edit_xml_path, :class => 'repair_images', :id => 'edit_xml_button'
+      link_hash = {:label => '.repair_images', :class => 'repair_images'}
     elsif @missing_images.include?(assembly[:image_uuid])
-      link_to t('.repair_images'), edit_xml_path, :class => 'repair_images', :id => 'edit_xml_button'
+      link_hash = {:label => '.repair_images', :class => 'repair_images'}
     else
-      link_to t('.images_valid'), edit_xml_path, :class => 'images_valid', :id => 'edit_xml_button'
+      link_hash = {:label => '.images_valid', :class => 'images_valid'}
+    end
+    if modify_perm
+      link_to(t(link_hash[:label]), edit_xml_path,
+              :class => link_hash[:class], :id => 'edit_xml_button')
+    else
+      content_tag(:a, t(link_hash[:label]), :class => link_hash[:class],
+                  :id => 'edit_xml_button')
     end
   end
 
