cb643d581523d69acdac8d36d5d937edac0900fe
Steve Linabery <slinaber@redhat.com>
2012-09-06 12:04:13 -0500

    Bug 852400 - aeolus-upgrade fails to start postgresql and conductor-delayed_job service
    The second stage of the current upgrade set leaves aeolus services running
    after running aeolus-configure. The correct behaviour is to leave services
    stopped as they are at the beginning of each upgrade stage. A future
    enhancement will prompt $user at end of upgrade process to optionally restart
    aeolus services.
    
    Signed-off-by: John Eckersberg <jeckersb@redhat.com>

--- src/upgrade-scripts/default/0002_run_aeolus-configure.sh
+++ src/upgrade-scripts/default/0002_run_aeolus-configure.sh
@@ -2,12 +2,12 @@
 
 #name: Execute aeolus-configure
 #apply: aeolus-conductor
-#description: Executes aeolus-configure. Note: this restarts aeolus services. Skip this step if you want to manually run aeolus-configure later.
+#description: Executes aeolus-configure, but stops aeolus services before exiting
 
 # default configuration values (should be the same as in our sysv init script)
 if [ -f /etc/sysconfig/aeolus-conductor ]; then . /etc/sysconfig/aeolus-conductor; fi
 
 aeolus-configure 2>&1
 ret_code=$?
-
+aeolus-services stop
 exit $ret_code
