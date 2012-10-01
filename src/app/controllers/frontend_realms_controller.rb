6f8b124c44ef85772e24e1345517dc758debf34a
Richard Su <rwsu@redhat.com>
2012-10-01 16:29:46 -0700

    Renamed RealmsController to FrontendRealmsController

---
+++ src/app/controllers/frontend_realms_controller.rb
@@ -0,0 +1,160 @@
+#
+#   Copyright 2011 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+
+class FrontendRealmsController < ApplicationController
+  before_filter :require_user
+  before_filter :load_realms, :only =>[:index, :show]
+
+  def index
+    @title = t('realms.realms')
+    clear_breadcrumbs
+    save_breadcrumb(frontend_realms_path)
+    set_admin_content_tabs 'frontend_realms'
+    respond_to do |format|
+      format.html
+      format.js { render :partial => 'list' }
+    end
+  end
+
+  def new
+    require_privilege(Privilege::CREATE, ProviderRealm)
+    @realm = FrontendRealm.new
+    load_backend_realms
+  end
+
+  def edit
+    require_privilege(Privilege::MODIFY, ProviderRealm)
+    @realm = FrontendRealm.find(params[:id])
+    @title = @realm.name
+    load_backend_realms
+  end
+
+  def update
+    require_privilege(Privilege::MODIFY, ProviderRealm)
+    @realm = FrontendRealm.find(params[:id])
+    @title = @realm.name || t("realms.realm")
+
+    if params[:commit] == "Reset"
+      redirect_to edit_realm_url(@realm) and return
+    end
+
+    if @realm.update_attributes(params[:frontend_realm])
+      flash[:notice] = t"realms.flash.notice.updated"
+      redirect_to frontend_realms_url and return
+    end
+
+    load_backend_realms
+    render :action => 'edit'
+  end
+
+  def create
+    require_privilege(Privilege::CREATE, ProviderRealm)
+    #@provider = Provider.find(params[:provider_id])
+    @realm = FrontendRealm.new(params[:frontend_realm])
+    if @realm.save
+      flash[:notice] = t"realms.flash.notice.added"
+      redirect_to realm_path(@realm)
+    else
+      load_backend_realms
+      render :new
+    end
+  end
+
+  def destroy
+    require_privilege(Privilege::MODIFY, ProviderRealm)
+    if FrontendRealm.destroy(params[:id])
+      flash[:notice] = t "realms.flash.notice.deleted"
+    else
+      flash[:error] = t"realms.flash.error.not_deleted"
+    end
+    redirect_to frontend_realms_path
+  end
+
+  def multi_destroy
+    deleted = []
+    not_deleted = []
+    if params[:realm_selected].blank?
+      flash[:error] = t"realms.flash.error.select_to_delete"
+    else
+      FrontendRealm.find(params[:realm_selected]).each do |realm|
+        require_privilege(Privilege::MODIFY, ProviderRealm)
+        if realm.destroy
+          deleted << realm.name
+        else
+          not_deleted << realm.name
+        end
+      end
+    end
+
+    unless deleted.empty?
+      flash[:notice] = "#{t('realms.flash.notice.more_deleted')} #{deleted.join(', ')}"
+    end
+    unless not_deleted.empty?
+      flash[:error] = "#{t('realms.flash.error.more_not_deleted')} #{not_deleted.join(', ')}"
+    end
+    redirect_to frontend_realms_path
+  end
+
+  def show
+    @realm = FrontendRealm.find(params[:id])
+    @title = @realm.name
+    @tab_captions = [t('realms.tab_captions.properties'), t('realms.tab_captions.mapping')]
+    @details_tab = params[:details_tab].blank? ? 'properties' : params[:details_tab]
+    @details_tab = 'properties' unless ['properties', 'mapping'].include?(@details_tab)
+
+    @backend_realm_targets = @realm.realm_backend_targets.select { |x| x.realm_or_provider_type == 'Realm' }
+    @backend_provider_targets = @realm.realm_backend_targets.select { |x| x.realm_or_provider_type == 'Provider' }
+
+    save_breadcrumb(frontend_realm_path(@realm), @realm.name)
+    load_backend_realms
+
+    respond_to do |format|
+      format.html { render :action => 'show' }
+      format.js do
+        if params.delete :details_pane
+          render :partial => 'layouts/details_pane' and return
+        end
+        render :partial => @details_tab
+      end
+      format.json { render :json => @realm }
+    end
+  end
+
+  def filter
+    redirect_to_original({"realms_preset_filter" => params[:realms_preset_filter], "realms_search" => params[:realms_search]})
+  end
+
+  protected
+
+  def load_backend_realms
+    #TODO: list only realms user has permission on
+    @backend_realms = Provider.list_for_user(current_session, current_user,
+                                             Privilege::USE).collect do |provider|
+      provider.provider_realms
+    end.flatten
+
+    @providers = Provider.list_for_user(current_session, current_user,
+                                        Privilege::USE)
+  end
+
+  def load_realms
+    @header = [
+      {:name => '', :sortable => false},
+      {:name => t("realms.index.realm_name"), :sort_attr => :name},
+    ]
+    @realms = FrontendRealm.apply_filters(:preset_filter_id => params[:realms_preset_filter], :search_filter => params[:realms_search])
+  end
+end
--- src/features/frontend_realm.feature
+++ src/features/frontend_realm.feature
@@ -1,4 +1,4 @@
-Feature: Manage Realms
+Feature: Manage Frontend Realms
   In order to manage realms
   As an admin
   I want to add/edit/remove realms
@@ -6,29 +6,29 @@ Feature: Manage Realms
   Background:
     Given I am an authorised user
     And I am logged in
-    And there's no realm
-    And a realm "testrealm1" exists
+    And there's no frontend realm
+    And a frontend realm "testrealm1" exists
 
   Scenario: Create new frontend realm
-    Given I am on the realms page
+    Given I am on the frontend realms page
     When I follow "new_realm_button"
-    Then I should be on the new realm page
+    Then I should be on the new frontend realm page
     When I fill in "frontend_realm_name" with "testrealm2"
     And I press "Save"
     Then I should see a confirmation message
 
   Scenario: Validate unique frontend realm name
-    Given I am on the realms page
-    And there is a realm "testrealm1"
+    Given I am on the frontend realms page
+    And there is a frontend realm "testrealm1"
     When I follow "new_realm_button"
-    Then I should be on the new realm page
+    Then I should be on the new frontend realm page
     When I fill in "frontend_realm_name" with "testrealm1"
     And I press "frontend_realm_submit"
     Then I should see an error message
 
   Scenario: Add realm mapping
-    Given I am on the realms page
-    And there is a realm "testrealm1"
+    Given I am on the frontend realms page
+    And there is a frontend realm "testrealm1"
     And a provider "mock_provider1" exists
     When I follow "testrealm1"
     And I follow "mapping_to_provider_button"
@@ -37,8 +37,8 @@ Feature: Manage Realms
     Then I should see a confirmation message
 
   Scenario: Change the name
-    Given I am on the realms page
-    And there is a realm "testrealm1"
+    Given I am on the frontend realms page
+    And there is a frontend realm "testrealm1"
     When I follow "testrealm1"
     And I follow "edit_realm_button"
     Then I should see "Properties"
@@ -48,38 +48,38 @@ Feature: Manage Realms
 
   Scenario: Show realm detials
     Given a realm "testrealm2" exists
-    And I am on the realms page
+    And I am on the frontend realms page
     When I follow "testrealm2"
-    Then I should be on testrealm2's realm page
+    Then I should be on testrealm2's frontend realm page
 
   Scenario: Delete realms
-    Given a realm "testrealm2" exists
-    And I am on the realms page
+    Given a frontend realm "testrealm2" exists
+    And I am on the frontend realms page
     And there are 2 realms
-    When I check "testrealm2" realm
-    And I check "testrealm1" realm
+    When I check "testrealm2" frontend realm
+    And I check "testrealm1" frontend realm
     And I press "delete_button"
     Then there should be only 0 realms
-    And I should be on the realms page
+    And I should be on the frontend realms page
     And I should see a confirmation message
 
   Scenario: Delete realm without selecting one
-    Given I am on the realms page
+    Given I am on the frontend realms page
     When I press "delete_button"
-    Then I should be on the realms page
+    Then I should be on the frontend realms page
     And I should see an error message
 
   Scenario: Delete realm mapping without selecting one
-    Given a realm "testrealm1" exists mapped to a provider "mock_provider1"
-    And I am on the realms page
+    Given a frontend realm "testrealm1" exists mapped to a provider "mock_provider1"
+    And I am on the frontend realms page
     When I follow "testrealm1"
     And I press "delete_mapping_button"
     Then I should see an error message
 
   Scenario: Search Realms
-    Given a realm "myrealm" exists
-    And a realm "somerealm" exists
-    And I am on the realms page
+    Given a frontend realm "myrealm" exists
+    And a frontend realm "somerealm" exists
+    And I am on the frontend realms page
     Then I should see "myrealm"
     And I should see "somerealm"
     When I fill in "realms_search" with "some"
