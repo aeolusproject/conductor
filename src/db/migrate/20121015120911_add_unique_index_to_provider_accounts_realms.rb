324b7f30f51780400171a790ab1f92addab26cbf
Martin Povolny <mpovolny@redhat.com>
2012-10-17 09:42:00 +0200

    fix migration to work on renamed table provider_accounts_provider_realms

--- src/db/migrate/20121015120911_add_unique_index_to_provider_accounts_realms.rb
+++ src/db/migrate/20121015120911_add_unique_index_to_provider_accounts_realms.rb
@@ -1,5 +1,5 @@
 class AddUniqueIndexToProviderAccountsRealms < ActiveRecord::Migration
   def change
-    add_index :provider_accounts_realms, [:provider_account_id, :realm_id], :unique => true, :name => 'provider_accounts_realms_unique_key'
+    add_index :provider_accounts_provider_realms, [:provider_account_id, :provider_realm_id], :unique => true, :name => 'provider_accounts_provider_realms_unique_key'
   end
 end
