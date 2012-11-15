eb5cf7a0de87cb27a532889f94a2fd5c22ea78a2
Jan Provaznik <jprovazn@redhat.com>
2012-11-15 13:37:48 +0100

    Tim - prefixed *_path and *_url helper method calls
    _path and _url method calls should be prefixed to distinguish between
    engine and main_app

--- src/config/navigation.rb
+++ src/config/navigation.rb
@@ -18,13 +18,13 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.autogenerate_item_ids = false
   navigation.items do |first_level|
     first_level.dom_class = 'container'
-    first_level.item :monitor, t('navigation.first_level.monitor'), pools_path, :class => 'monitor', :link => { :id => 'monitor' }, :highlights_on => /\/deployments|\/pools$|\/pools\/\d$|\/pools\?|\/instances|\/logs|\/\z/
-    first_level.item :administer, t('navigation.first_level.administer'), users_path, :class => 'administer' do |second_level|
-      second_level.item :users, t('navigation.second_level.users'), users_path, :link => { :class => 'users' }, :highlights_on => /\/users|\/user_groups|\/roles|\/permissions|\/account/
-      second_level.item :environments, t('navigation.second_level.environments'), pool_families_path, :link => { :class => 'environments' }, :highlights_on => /\/pool_families|\/images|\/pools\/\d\/provider_selection/
-      second_level.item :content, t('navigation.second_level.content'), catalogs_path, :link => { :class => 'content' }, :highlights_on => /\/catalogs|\/catalog_entries|\/realms|\/hardware_profiles|\/realm_mappings|\/deployables/
-      second_level.item :cloud_providers, t('navigation.second_level.cloud_providers'), providers_path, :link => { :class => 'providers' }, :highlights_on => /\/providers|\/provider_realms|\/config_servers/
-      second_level.item :settings, t('navigation.second_level.settings'), settings_path, :link => { :class => 'settings'}, :highlights_on => /\/settings/
+    first_level.item :monitor, t('navigation.first_level.monitor'), main_app.pools_path, :class => 'monitor', :link => { :id => 'monitor' }, :highlights_on => /\/deployments|\/pools$|\/pools\/\d$|\/pools\?|\/instances|\/logs|\/\z/
+    first_level.item :administer, t('navigation.first_level.administer'), main_app.users_path, :class => 'administer' do |second_level|
+      second_level.item :users, t('navigation.second_level.users'), main_app.users_path, :link => { :class => 'users' }, :highlights_on => /\/users|\/user_groups|\/roles|\/permissions|\/account/
+      second_level.item :environments, t('navigation.second_level.environments'), main_app.pool_families_path, :link => { :class => 'environments' }, :highlights_on => /\/pool_families|\/images|\/pools\/\d\/provider_selection/
+      second_level.item :content, t('navigation.second_level.content'), main_app.catalogs_path, :link => { :class => 'content' }, :highlights_on => /\/catalogs|\/catalog_entries|\/realms|\/hardware_profiles|\/realm_mappings|\/deployables/
+      second_level.item :cloud_providers, t('navigation.second_level.cloud_providers'), main_app.providers_path, :link => { :class => 'providers' }, :highlights_on => /\/providers|\/provider_realms|\/config_servers/
+      second_level.item :settings, t('navigation.second_level.settings'), main_app.settings_path, :link => { :class => 'settings'}, :highlights_on => /\/settings/
     end
   end
 end
