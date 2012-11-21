a318a1b7ad321491dd1a4f589eddb5a7c10bf5a3
Jan Provaznik <jprovazn@redhat.com>
2012-11-21 14:48:40 +0100

    Tim - added base_images and templates controller permission tests

---
+++ src/spec/factories/tim/base_image.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :base_image, :class => Tim::BaseImage do
+    name "test base image"
+    description "description of test image"
+    #association :pool_family
+    # make sure that template and image have same pool family
+    pool_family { template ? template.pool_family : create(:pool_family)}
+  end
+
+  factory :base_image_with_template, :parent => :base_image do
+    association :template, :factory => :template
+  end
+end
