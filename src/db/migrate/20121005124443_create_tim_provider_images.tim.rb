45a91b13326bb18031049aba56093619991cd6dc
Jan Provaznik <jprovazn@redhat.com>
2012-11-21 14:48:37 +0100

    Tim - added gem, migrations and mountpoint

---
+++ src/db/migrate/20121005124443_create_tim_provider_images.tim.rb
@@ -0,0 +1,20 @@
+# This migration comes from tim (originally 20120911204329)
+class CreateTimProviderImages < ActiveRecord::Migration
+  def change
+    create_table :tim_provider_images do |t|
+      t.integer :id
+      t.string :factory_id
+      t.integer :target_image_id
+      t.string :provider
+      # The Cloud Specific ID. i.e. ami-12345678
+      t.string :external_image_id
+      t.integer :provider_account_id
+      t.boolean :snapshot
+
+      t.string :status
+      t.string :status_detail
+      t.string :progress # Factory Target Image percent_complete
+      t.timestamps
+    end
+  end
+end
