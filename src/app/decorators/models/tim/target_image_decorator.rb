8c1b30e4ebde26c864a8c849b31398c83a350d13
Jan Provaznik <jprovazn@redhat.com>
2012-11-23 17:23:10 +0100

    Tim - replaced iwhd calls by Tim calls

--- src/app/decorators/models/tim/target_image_decorator.rb
+++ src/app/decorators/models/tim/target_image_decorator.rb
@@ -7,4 +7,12 @@ Tim::TargetImage.class_eval do
   def base_image
     image_version.base_image
   end
+
+  private
+
+  def set_target
+    # TODO: codenames have changed in new imagefactory
+    #@target = provider_type.name
+    @target = 'MockSphere'
+  end
 end
