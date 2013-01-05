810c42b6579821b23ebc988dd8e5f7db27ef0244
Jan Provaznik <jprovazn@redhat.com>
2013-01-05 14:04:51 +0100

    Fixed creation of new image
    Removed creation of template from url/uploaded file from base image model.
    Created DownloadService for downloading files from url.

--- src/app/decorators/models/tim/base_image_decorator.rb
+++ src/app/decorators/models/tim/base_image_decorator.rb
@@ -17,8 +17,6 @@
 Tim::BaseImage.class_eval do
   include PermissionedObject
 
-  attr_reader :template_url, :template_file
-
   belongs_to :pool_family
   has_many :permissions, :as => :permission_object, :dependent => :destroy,
            :include => [:role],
@@ -38,14 +36,6 @@ Tim::BaseImage.class_eval do
   # More human readable translations are defined in localization file.
   def self.human_attribute_name(attr, opts = {})
     I18n.t("tim.base_images.import.#{attr}", :default => opts[:default])
-  end
-
-  def template_url=(url)
-    init_template(import_xml_from_url(url))
-  end
-
-  def template_file=(file)
-    init_template(file.read)
   end
 
   def perm_ancestors
@@ -69,34 +59,6 @@ Tim::BaseImage.class_eval do
   end
 
   private
-
-  def init_template(xml)
-    self.template = Tim::Template.new(
-      :xml         => xml,
-      :pool_family => pool_family
-    )
-  end
-
-  #TODO: DRY this, taken from application controller
-  def import_xml_from_url(url)
-    if url.blank?
-      errors.add(:base, I18n.t('application_controller.flash.error.no_url_provided'))
-    elsif not url =~ URI::regexp
-      errors.add(:base, I18n.t('application_controller.flash.error.not_valid_url', :url => url))
-    else
-      begin
-        response = RestClient.get(url, :accept => :xml)
-        if response.code == 200
-          return response
-        else
-          errors.add(:base, I18n.t('application_controller.flash.error.download_failed'))
-        end
-      rescue RestClient::Exception, SocketError, URI::InvalidURIError, Errno::ECONNREFUSED, Errno::ETIMEDOUT
-        errors.add(:base, I18n.t('application_controller.flash.error.not_valid_or_reachable', :url => url))
-      end
-    end
-    return nil
-  end
 
   def generate_uuid
     self[:uuid] = UUIDTools::UUID.timestamp_create.to_s
