472a1cf9b2d96827510302117b10cb430a102406
Martin Povolny <mpovolny@redhat.com>
2013-01-28 19:27:33 +0100

    remove all foreign keys until rails is fixed https://github.com/rails/rails/pull/8548

--- src/db/migrate/20130116112862_add_keys_pools.rb
+++
@@ -1,6 +0,0 @@
-class AddKeysPools < ActiveRecord::Migration
-  def change
-    add_foreign_key "pools", "pool_families", :name => "pools_pool_family_id_fk"
-    add_foreign_key "pools", "quotas", :name => "pools_quota_id_fk"
-  end
-end
--- src/db/migrate/20130116112863_add_keys_pool_families.rb
+++
@@ -1,5 +0,0 @@
-class AddKeysPoolFamilies < ActiveRecord::Migration
-  def change
-    add_foreign_key "pool_families", "quotas", :name => "pool_families_quota_id_fk"
-  end
-end
