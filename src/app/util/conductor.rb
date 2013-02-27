4bd44197af74bd50127ca3e4ac3dd1f2726500e8
Imre Farkas <ifarkas@redhat.com>
2013-02-27 11:50:23 +0100

    Remove unused utils/conductor code

--- src/app/util/conductor.rb
+++ src/app/util/conductor.rb
@@ -14,35 +14,6 @@
 #   limitations under the License.
 #
 
-require 'i18n'
-
-def gb_to_kb(val_in_gigs)
-  return nil if nil_or_empty(val_in_gigs)
-  return val_in_gigs.to_i * 1024 * 1024
-end
-
-def kb_to_gb(val_in_kb)
-  return nil if nil_or_empty(val_in_kb)
-  return val_in_kb.to_i / 1024 / 1024
-end
-def mb_to_kb(val_in_mb)
-  return nil if nil_or_empty(val_in_mb)
-  return val_in_mb.to_i * 1024
-end
-
-def kb_to_mb(val_in_kb)
-  return nil if nil_or_empty(val_in_kb)
-  return val_in_kb.to_i / 1024
-end
-
-def nil_or_empty(val)
-  if val.nil? or (val.kind_of?(String) and val.empty?)
-    return true
-  else
-    return false
-  end
-end
-
 # Try to clean up and internationalize certain errors we get from other components
 # Accepts a string or an Exception
 def humanize_error(error, options={})
@@ -61,15 +32,15 @@ end
 
 # Find a class for CSV generation
 def get_csv_class
-   return FasterCSV if Object.const_defined?(:FasterCSV)
-   Object.const_defined?(:CSV) or require 'csv'
-   CSV
+  return FasterCSV if Object.const_defined?(:FasterCSV)
+  Object.const_defined?(:CSV) or require 'csv'
+  CSV
 end
 
 # log exception and backtrace
 def log_backtrace(exception, message=nil, severity=:error)
-    Rails.logger.send(severity, message.to_s.empty? ? exception.message :
-                        "#{message}: #{exception.message}")
-    Rails.logger.send(severity, 'backtrace follows')
-    Rails.logger.send(severity, exception.backtrace.join("\n\t"))
+  Rails.logger.send(severity, message.to_s.empty? ? exception.message :
+                      "#{message}: #{exception.message}")
+  Rails.logger.send(severity, 'backtrace follows')
+  Rails.logger.send(severity, exception.backtrace.join("\n\t"))
 end
